7b123f27d0c8eb5be0c507b53fa50fc1bb7712ed
MaheepK <maheepk@MaheepKs-MacBook-Pro.local>
2015-06-29 21:22:30 +0530

    New updates

--- Calculator Swift/ViewController.swift
+++ Calculator Swift/ViewController.swift
@@ -3,7 +3,7 @@
 //  Calculator Swift
 //
 //  Created by MaheepK on 24/06/15.
-//  Copyright (c) 2015 Getondemand. All rights reserved.
+//  Copyright (c) 2015 CrayonLabs. All rights reserved.
 //
 
 import UIKit
